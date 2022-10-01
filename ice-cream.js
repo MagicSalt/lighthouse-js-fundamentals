96a4077042dcd76e8b546b0cd850e96ffe8c0c11
Darren Lin <darrenlin815@hotmail.com>
2022-09-30 22:58:21 -0700

    Updated exercises

---
+++ ice-cream.js
@@ -0,0 +1,19 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push('root beer');
+
+// Checking to see that 'root beer' has been added to the array
+console.log(iceCreamFlavours)
+
+// console.log the first flavour in the array
+console.log(iceCreamFlavours[0])
+
+
+// console.log the last flavour in the array
+console.log(iceCreamFlavours[5])
+
+// console.log the total number of flavours in the array
+console.log(iceCreamFlavours.length);
