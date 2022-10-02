757df1273e57d576d5d59eb6fd21e7a1d5827afc
Darren Lin <darrenlin815@hotmail.com>
2022-10-02 00:10:25 -0700

    Using objects and functions to sort garbage

---
+++ smart-garbage.js
@@ -0,0 +1,16 @@
+const bins = {
+  waste: 4,
+  recycling: 2,
+  compost: 5
+}
+
+const trash = 'recycling'
+
+function smartGarbage (trash, bins) {
+  if (trash === 'waste') bins.waste++
+  else if (trash === 'recycling') bins.recycling++
+  else if (trash === 'compost') bins.compost++
+  return bins
+}
+
+console.log(smartGarbage(trash, bins))
